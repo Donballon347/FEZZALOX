106ec50d5f6ee38280d23f74827d88242f3f822a
Arthur <90266484+Donballon347@users.noreply.github.com>
2023-04-22 16:53:38 +0300

    Add files via upload

---
+++ Railway/src/main/java/org/example/ArrayHaul.java
@@ -0,0 +1,12 @@
+package org.example;
+import java.util.ArrayList;
+
+
+public class ArrayHaul {
+    private ArrayList<Haul> arrayHauls;
+    public ArrayHaul () {
+        arrayHauls = new ArrayList<Haul>();
+    }
+
+    FEZZA ЛОХ ОБЕЛСЯ БЛОХ СЕЛ НА ЛАВОЧКУ И СДОХ, А ПОД ЛАВОЧКОЙ ЗМЕЯ ОТКУСИЛА ПОЛ Х*Я
+}
